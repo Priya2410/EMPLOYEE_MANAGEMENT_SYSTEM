e003d9fdaa5e7d4e30743733920af1650e7ab52d
Priya Mohata <apple@Apples-MacBook-Air.local>
2022-04-17 19:47:35 +0530

    Added home page , login , insertion

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * @author Faheem Irfan
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DatabaseConnection {
+    
+ final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+ final static String DB_URL = "jdbc:mysql://localhost:3306/employee";
+    
+ final static String USER = "root";
+ final static String PASS = "";
+    
+ 
+    
+    public static Connection connection(){
+        try
+        {
+            Class.forName(JDBC_DRIVER); 
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            return conn;
+            
+        }catch(Exception e){ JOptionPane.showMessageDialog(null, e);
+        return null;
+        }
+     
+        
+        
+    }
+}
